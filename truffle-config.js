2f1c9eb1c1002748252502ed5e9c1cdd0ae6c631
mcmanyika <mcmanyika.com>
2021-07-16 14:08:42 -0500

    commit_message

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const mnemonic = "olympic squeeze extra where catalog elegant coin snack vintage funny kind solve"
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const mnemonic = "olympic squeeze extra where catalog elegant coin snack vintage funny kind solve"
 module.exports = {
   networks: {
     development: {
@@ -10,14 +10,14 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/16697f3c94754b18828da39e491a4ad1`),
-      network_id: 4,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    },
+    // rinkeby: {
+    //   provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/16697f3c94754b18828da39e491a4ad1`),
+    //   network_id: 4,       // Ropsten's id
+    //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    //   confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
