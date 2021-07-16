061cf291aff9a470dca98ce835d04245406a68ad
mcmanyika <mcmanyika.com>
2021-07-16 07:44:24 -0500

    Updates

---
+++ precache-manifest.373b7288de23ff87280a6e067e8f0035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26e7365adfce15fa38f1",
+    "url": "/social-network/static/css/main.74811130.chunk.css"
+  },
+  {
+    "revision": "26e7365adfce15fa38f1",
+    "url": "/social-network/static/js/main.26e7365a.chunk.js"
+  },
+  {
+    "revision": "00a7cbdca77e938d85d1",
+    "url": "/social-network/static/css/1.30b840f9.chunk.css"
+  },
+  {
+    "revision": "00a7cbdca77e938d85d1",
+    "url": "/social-network/static/js/1.00a7cbdc.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "e8e6aac5f77f872cf119c24a6ed582ab",
+    "url": "/social-network/index.html"
+  }
+];
